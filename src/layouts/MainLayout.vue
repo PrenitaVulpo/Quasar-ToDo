15670207e4a74674157b4f7e72f5ac8aadfabb98
PrenitaVulpo <guilhermepdl97@protonmail.com>
2021-07-22 23:55:36 -0300

    add header content

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,119 +1,111 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
-    <q-header elevated>
-      <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
+	<q-layout view="lHh Lpr lFf">
+		<q-header>
+			<q-toolbar>
+				<q-btn
+					flat
+					dense
+					round
+					icon="menu"
+					aria-label="Menu"
+					@click="toggleLeftDrawer"
+				/>
+			</q-toolbar>
+			<div class="q-px-lg q-pt-xl q-mb-md">
+				<span class="text-h3">ToDo</span>
+				<span class="text-subtitle1">{{
+					Intl.DateTimeFormat('pt-br').format(new Date())
+				}}</span>
+			</div>
+		</q-header>
 
-        <q-toolbar-title>
-          Quasar App
-        </q-toolbar-title>
+		<q-drawer v-model="leftDrawerOpen" show-if-above bordered class="bg-grey-1">
+			<q-list>
+				<q-item-label header class="text-grey-8">
+					Essential Links
+				</q-item-label>
 
-        <div>Quasar v{{ $q.version }}</div>
-      </q-toolbar>
-    </q-header>
+				<EssentialLink
+					v-for="link in essentialLinks"
+					:key="link.title"
+					v-bind="link"
+				/>
+			</q-list>
+		</q-drawer>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-      class="bg-grey-1"
-    >
-      <q-list>
-        <q-item-label
-          header
-          class="text-grey-8"
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in essentialLinks"
-          :key="link.title"
-          v-bind="link"
-        />
-      </q-list>
-    </q-drawer>
-
-    <q-page-container>
-      <router-view />
-    </q-page-container>
-  </q-layout>
+		<q-page-container>
+			<router-view />
+		</q-page-container>
+	</q-layout>
 </template>
 
 <script lang="ts">
-import EssentialLink from 'components/EssentialLink.vue'
+import EssentialLink from 'components/EssentialLink.vue';
 
 const linksList = [
-  {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
-  },
-  {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
-  },
-  {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
-  },
-  {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
-  },
-  {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
-  },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
+	{
+		title: 'Docs',
+		caption: 'quasar.dev',
+		icon: 'school',
+		link: 'https://quasar.dev',
+	},
+	{
+		title: 'Github',
+		caption: 'github.com/quasarframework',
+		icon: 'code',
+		link: 'https://github.com/quasarframework',
+	},
+	{
+		title: 'Discord Chat Channel',
+		caption: 'chat.quasar.dev',
+		icon: 'chat',
+		link: 'https://chat.quasar.dev',
+	},
+	{
+		title: 'Forum',
+		caption: 'forum.quasar.dev',
+		icon: 'record_voice_over',
+		link: 'https://forum.quasar.dev',
+	},
+	{
+		title: 'Twitter',
+		caption: '@quasarframework',
+		icon: 'rss_feed',
+		link: 'https://twitter.quasar.dev',
+	},
+	{
+		title: 'Facebook',
+		caption: '@QuasarFramework',
+		icon: 'public',
+		link: 'https://facebook.quasar.dev',
+	},
+	{
+		title: 'Quasar Awesome',
+		caption: 'Community Quasar projects',
+		icon: 'favorite',
+		link: 'https://awesome.quasar.dev',
+	},
 ];
 
-import { defineComponent, ref } from 'vue'
+import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
-  name: 'MainLayout',
+	name: 'MainLayout',
 
-  components: {
-    EssentialLink
-  },
+	components: {
+		EssentialLink,
+	},
 
-  setup () {
-    const leftDrawerOpen = ref(false)
+	setup() {
+		const leftDrawerOpen = ref(false);
 
-    return {
-      essentialLinks: linksList,
-      leftDrawerOpen,
-      toggleLeftDrawer () {
-        leftDrawerOpen.value = !leftDrawerOpen.value
-      }
-    }
-  }
-})
+		return {
+			essentialLinks: linksList,
+			leftDrawerOpen,
+			toggleLeftDrawer() {
+				leftDrawerOpen.value = !leftDrawerOpen.value;
+			},
+		};
+	},
+});
 </script>

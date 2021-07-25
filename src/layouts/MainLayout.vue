974f9621a0e27db67a0e24ceb98c5c24de6ed2c2
PrenitaVulpo <guilhermepdl97@protonmail.com>
2021-07-24 21:12:01 -0300

    changes to the drawer

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -18,18 +18,66 @@
 			</div>
 		</q-header>
 
-		<q-drawer v-model="leftDrawerOpen" show-if-above bordered class="bg-grey-1">
-			<q-list>
-				<q-item-label header class="text-grey-8">
-					Essential Links
-				</q-item-label>
+		<q-drawer
+			v-model="leftDrawerOpen"
+			show-if-above
+			:width="200"
+			:breakpoint="600"
+		>
+			<q-scroll-area
+				style="
+					height: calc(100% - 192px);
+					margin-top: 192px;
+					border-right: 1px solid #ddd;
+				"
+			>
+				<q-list padding>
+					<q-item clickable v-ripple>
+						<q-item-section avatar>
+							<q-icon name="inbox" />
+						</q-item-section>
 
-				<EssentialLink
-					v-for="link in essentialLinks"
-					:key="link.title"
-					v-bind="link"
-				/>
-			</q-list>
+						<q-item-section> Inbox </q-item-section>
+					</q-item>
+
+					<q-item active clickable v-ripple>
+						<q-item-section avatar>
+							<q-icon name="star" />
+						</q-item-section>
+
+						<q-item-section> Star </q-item-section>
+					</q-item>
+
+					<q-item clickable v-ripple>
+						<q-item-section avatar>
+							<q-icon name="send" />
+						</q-item-section>
+
+						<q-item-section> Send </q-item-section>
+					</q-item>
+
+					<q-item clickable v-ripple>
+						<q-item-section avatar>
+							<q-icon name="drafts" />
+						</q-item-section>
+
+						<q-item-section> Drafts </q-item-section>
+					</q-item>
+				</q-list>
+			</q-scroll-area>
+
+			<q-img
+				class="absolute-top"
+				src="https://cdn.quasar.dev/img/material.png"
+				style="height: 192px"
+			>
+				<div class="absolute-bottom bg-transparent">
+					<q-avatar size="56px" class="q-mb-sm">
+						<img src="../assets/user.png" />
+					</q-avatar>
+					<div class="text-weight-bold">Usuário anônimo</div>
+				</div>
+			</q-img>
 		</q-drawer>
 
 		<q-page-container>
@@ -41,66 +89,17 @@
 <script lang="ts">
 import EssentialLink from 'components/EssentialLink.vue';
 import { date } from 'quasar';
-
-const linksList = [
-	{
-		title: 'Docs',
-		caption: 'quasar.dev',
-		icon: 'school',
-		link: 'https://quasar.dev',
-	},
-	{
-		title: 'Github',
-		caption: 'github.com/quasarframework',
-		icon: 'code',
-		link: 'https://github.com/quasarframework',
-	},
-	{
-		title: 'Discord Chat Channel',
-		caption: 'chat.quasar.dev',
-		icon: 'chat',
-		link: 'https://chat.quasar.dev',
-	},
-	{
-		title: 'Forum',
-		caption: 'forum.quasar.dev',
-		icon: 'record_voice_over',
-		link: 'https://forum.quasar.dev',
-	},
-	{
-		title: 'Twitter',
-		caption: '@quasarframework',
-		icon: 'rss_feed',
-		link: 'https://twitter.quasar.dev',
-	},
-	{
-		title: 'Facebook',
-		caption: '@QuasarFramework',
-		icon: 'public',
-		link: 'https://facebook.quasar.dev',
-	},
-	{
-		title: 'Quasar Awesome',
-		caption: 'Community Quasar projects',
-		icon: 'favorite',
-		link: 'https://awesome.quasar.dev',
-	},
-];
-
 import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
 	name: 'MainLayout',
 
-	components: {
-		EssentialLink,
-	},
+	components: {},
 
 	setup() {
 		const leftDrawerOpen = ref(false);
 
 		return {
-			essentialLinks: linksList,
 			leftDrawerOpen,
 			toggleLeftDrawer() {
 				leftDrawerOpen.value = !leftDrawerOpen.value;
@@ -110,7 +109,7 @@ export default defineComponent({
 	computed: {
 		formatedDate() {
 			let todayDate = new Date();
-			return date.formatDate(todayDate, 'HH:mm:ss DD-MM-YYYY');
+			return date.formatDate(todayDate, 'dddd, D MMMM YYYY');
 		},
 	},
 });
